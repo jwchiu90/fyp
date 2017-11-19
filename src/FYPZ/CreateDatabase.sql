e17e3baec644500781d76f321db784961623dda8
15007988 <15007988@PFT-15007988>
2017-11-19 18:11:36 +0800

    Please enter the commit message for your changes. Lines starting

--- src/FYPZ/CreateDatabase.sql
+++ src/FYPZ/CreateDatabase.sql
@@ -26,5 +26,31 @@ CREATE TABLE Message(
 
     Group_id INT FOREIGN KEY 
     REFERENCES Groups (Group_id)
-
 );
+
+CREATE TABLE Contact(
+  Contact_id INT NOT NULL PRIMARY KEY,
+  Group_id VARCHAR(45) NOT NULL,
+  Name VARCHAR(45) NULL,
+  Email VARCHAR(45) NULL,
+ );
+
+ CREATE TABLE Template (
+  Template_id INT NOT NULL PRIMARY KEY,
+  Name VARCHAR(45) NULL,
+  Content VARCHAR(45) NULL,
+  Message_id VARCHAR(45) NOT NULL,
+  );
+
+CREATE TABLE C_Message (
+  C_Message_id INT NOT NULL PRIMARY KEY,
+ 
+ Contact_id INT FOREIGN KEY
+    REFERENCES Contact (Contact_id),
+
+  Message_id INT FOREIGN KEY 
+    REFERENCES Message (Message_id)
+);
+
+
+
