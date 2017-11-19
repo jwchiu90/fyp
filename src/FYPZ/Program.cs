98fb5680be1819b7f9242acf6a303073db36d6f7
jwchiu90 <33799556+jwchiu90@users.noreply.github.com>
2017-11-19 16:53:55 +0800

    kitty

--- src/FYPZ/Program.cs
+++ src/FYPZ/Program.cs
@@ -11,7 +11,7 @@ namespace FYPZ
     {
         public static void Main(string[] args)
         {
-            var hello = "Hello World";
+            var hello = "Hello kitty";
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
